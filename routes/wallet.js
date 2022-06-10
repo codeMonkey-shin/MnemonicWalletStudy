1fabeba40893afa5a363d9086bb7400f8fc96627
codeMonkey-shin <sdw0711@hanmail.net>
2022-06-10 12:16:37 +0900

    demo

--- routes/wallet.js
+++ routes/wallet.js
@@ -4,19 +4,19 @@ const lightwallet = require("eth-lightwallet");
 const fs = require('fs');
 
 // TODO : lightwallet 모듈을 사용하여 랜덤한 니모닉 코드를 얻습니다.
-router.post('/newMnemonic', async(req,res) => {
+router.post('/newMnemonic', async (req, res) => {
     let mnemonic;
     try {
         mnemonic = lightwallet.keystore.generateRandomSeed();
         res.json({mnemonic});
-    } catch(err) {
+    } catch (err) {
         console.log(err);
     }
 });
 
 
 // TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
-router.post('/newWallet', async(req, res) => {
+router.post('/newWallet', async (req, res) => {
     let password = req.body.password
     let mnemonic = req.body.mnemonic;
 
@@ -30,15 +30,14 @@ router.post('/newWallet', async(req, res) => {
             function (err, ks) {
                 ks.keyFromPassword(password, function (err, pwDerivedKey) {
                     ks.generateNewAddress(pwDerivedKey, 1);
-
                     let address = (ks.getAddresses()).toString();
                     let keystore = ks.serialize();
-
-                    fs.writeFile('wallet.json',keystore,function(err,data){
-                        if(err) {
-                            res.json({code:999,message:"실패"});
+                    fs.writeFile('wallet.json', keystore, function (err, data) {
+                        if (err) {
+                            res.json({code: 999, message: "실패"});
+                            console.log(address)
                         } else {
-                            res.json({code:1,message:"성공"});
+                            res.json({code: 1, message: "성공", address: address, keystore: keystore});
                         }
                     });
                 });
